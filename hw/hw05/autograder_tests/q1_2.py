c74b594d6700ede4a7412574ea91ee25c5f37409
kayvan Momeni <kayvan@polygonsmedia.com>
2021-07-18 22:37:56 -0700

    Add files via upload

---
+++ hw/hw05/autograder_tests/q1_2.py
@@ -0,0 +1,34 @@
+test = {   'name': 'q1_2',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> final_scores.num_columns\n'
+                                               '3',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> set(['Opponent', 'Cal "
+                                               "Score', 'Opponent Score']) == "
+                                               'set(final_scores.labels)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> final_scores.take(range(5, '
+                                               '11))\n'
+                                               'Opponent         | Cal Score | '
+                                               'Opponent Score\n'
+                                               'Oregon           | 7         | '
+                                               '17\n'
+                                               'Oregon State     | 17        | '
+                                               '21\n'
+                                               'Utah             | 0         | '
+                                               '35\n'
+                                               'Washington State | 33        | '
+                                               '20\n'
+                                               'USC              | 17        | '
+                                               '41\n'
+                                               'Stanford         | 24        | '
+                                               '20',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw05/autograder_tests/q1_3.py
@@ -0,0 +1,16 @@
+test = {   'name': 'q1_3',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': ">>> 'did_cal_win' in "
+                                               'globals()\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'did_cal_win(final_scores.row(1))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
