90817a19ad5291e4f922cb684bcd83c2797c3a99
kayvan Momeni <kayvan@polygonsmedia.com>
2021-09-23 06:05:27 -0700

    Add files via upload

---
+++ exams/project1/autograder_tests/q1_14.py
@@ -0,0 +1,18 @@
+test = {   'name': 'q1_14',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> # Please use a list of '
+                                               'integers from 1 to 5;\n'
+                                               '>>> all(x in range(1, 6) for x '
+                                               'in set(scatter_statements))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> set(scatter_statements) == '
+                                               '{1, 3, 4}\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ exams/project1/autograder_tests/q1_9.py
@@ -0,0 +1,17 @@
+test = {   'name': 'q1_9',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> # Please use a list of '
+                                               'integers from 1 to 6;\n'
+                                               '>>> all(x in range(1, 7) for x '
+                                               'in set(fertility_statements))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> set(fertility_statements)\n'
+                                               '{1, 3, 4, 5, 6}',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
