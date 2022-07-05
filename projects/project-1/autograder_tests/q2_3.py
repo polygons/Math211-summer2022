2bbe1bc9ceb6c4768286d6567e37147d8cb0a63a
kayvan Momeni <kayvan@polygonsmedia.com>
2022-07-05 13:49:08 -0700

    Add files via upload

---
+++ projects/project-1/autograder_tests/q2_3.py
@@ -0,0 +1,17 @@
+test = {   'name': 'q2_3',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> 10 <= poverty_percent <= '
+                                               '20\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'np.allclose(np.round(poverty_percent, '
+                                               '2), 14.3)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
